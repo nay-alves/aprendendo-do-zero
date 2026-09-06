1efda191dd8bc38acf833fa4661fe6ae7ed963fe
nay-alves <naielly7777@gmail.com>
2026-09-06 00:42:22 -0300

    Primeiro envio dos meus códigos

---
+++ segundo/entradaDedados.py
@@ -0,0 +1,25 @@
+"Exercício 1: Saudação personalizada"
+#print("ola, " + input("qual é seu nome ? "))
+
+#meu# nome = input("qual seu nome ? ")
+
+# print(f"ola , {nome} seja bem vindo!")
+
+"Exercício 2: Historinha"
+#print("Um " + input("Digite um animal: ") + " foi até a " + input("Digite um lugar: ") + " e encontrou um(a) " + input("Digite um objeto: ") + " mágico!")
+
+#meu# animal = input("digite um animal: ")
+# lugar = input("digite um lugar: ")
+# objeto = input("digite um objeto: ")
+
+# print(f"um {animal} foi ate a {lugar} e encontrou um {objeto}!")
+
+"Exercício 3: Receita Maluca"
+#print("Com apenas " + input("Digite um ingrediente: ") + ", um " + input("Digite um utensílio: ") + " e um toque " + input("Digite um adjetivo: ") + ", você criou a receita mais inesperada do mundo!")
+
+#meu# ingredientes = input("quais ingredientes para o bolo? ")
+# utensilio = input("escolha apenas um utensilio que voce vai usar: ")
+# adjetivo = input("o seu bolo é ? ")
+
+# print(f"em meu bolo vai ter {ingredientes} e irei usar apenas {utensilio} para meu bolo ser {adjetivo} !")
+
---
+++ terceiro/variaveis.py
@@ -0,0 +1,35 @@
+"Exercício 2: Ficha Completa"
+# nome = input("Nome: ")
+# sobrenome = input("Sobrenome: ")
+# data_nascimento = input("Data de Nascimento: ")
+
+# print(f"{nome} {sobrenome} nascido(a) em {data_nascimento}")
+
+#meu # nome = input("qual é o seu nome? : ")
+# sobrenome = input("seu primeiro sobrenome : ")
+# nascimento = input("data de nascimento : ")
+
+# print(f"{nome}  {sobrenome}  {nascimento}")
+
+# print(f"cliente {nome} {sobrenome} nascido/a em {nascimento}")
+
+"Exercício 3: Gerador de personagens"
+# nome = input("Nome do personagem: ")
+# raca = input("Raça: ")
+# habilidade = input("Habilidade especial: ")
+
+# print(f"{nome}, a(o) {raca}, possui a habilidade de {habilidade}. Uma nova aventura está prestes a começar!")
+
+#meu #personagem = input("Nome do personagem: ")
+# comunidade  = input("raca: ")
+# habilidade  = input("Habilidade escolhida: ")
+
+#primeira opcao 
+# print(f"""
+#       nome:{personagem} 
+#       raca:{comunidade} 
+#       habilidade:{habilidade}""")
+
+#segunda opcao 
+# print(f"nome{personagem}\n raca:{comunidade}\n habilidade:{habilidade}")
+
