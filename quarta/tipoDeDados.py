53629c396e3a372e0d6103afb8ffa9793105a007
nay-alves <naielly7777@gmail.com>
2026-09-07 22:23:19 -0300

    segundo envio

---
+++ quarta/tipoDeDados.py
@@ -0,0 +1,45 @@
+"Exercício 1: Você não me parece velho(a)"
+# ano_nascimento = int(input("Digite o ano de nascimento: "))
+# ano_atual = 2025
+# idade = ano_atual - ano_nascimento
+
+# print(f"Você tem {idade} anos!")
+
+# meu
+# usario = int(input("ano de nasciment : "))
+# ano_atual = 2026
+
+# resultado = ano_atual - usario
+
+# print(f"sua idade é {resultado}")
+
+"Exercício 2: Posso dar o troco em bala?"
+# valor_compra = float(input("Qual é o valor da compra? "))
+# valor_pago = float(input("Quanto o cliente pagou? "))
+
+# troco = valor_pago - valor_compra
+
+# # usamos o .2f para formatar o valor com duas casas decimais
+# # mas não é obrigatório usar
+# print(f"O valor do troco é: {troco:.2f}")
+
+# meu
+# valor = float(input("qual o valor da compra? "))
+# cliente = float(input("cliente pagou? "))
+
+# troco = cliente - valor
+
+# print(f"o valor do troco é {troco}")
+
+"Exercício 3: Eu te juro que o código está certo!"
+
+# n1 = input("Primeiro número: ")
+# n2 = input("Segundo número: ")
+# soma = n1 + n2
+# print(f"O resultado da soma é: {soma}")
+
+# arrumando
+n1 = int(input("Primeiro número: "))
+n2 = int(input("Segundo número: "))
+soma = n1 + n2
+print(f"O resultado da soma é: {soma}")
---
+++ quinta/booleanoCondicionais.py
@@ -0,0 +1,63 @@
+"Exercício 1: Esse brinquedo é para crianças"
+# idade = int(input("Digite sua idade: "))
+# idade_limite = 12
+
+# if idade <= idade_limite:
+#   print("Você pode brincar!")
+# else:
+#   print("Você não pode brincar!")
+
+# meu
+# usuario = int(input("qual a sua idade? "))
+# faixa_etária = 12
+
+# if usuario <= faixa_etária:
+#     print("voce pode brincar!")
+# else :
+#     print("voce nao pode brincar :/")
+
+"Exercício 2: Só para os marombas altos"
+# altura = float(input("Qual é a sua altura? "))
+# if altura >= 1.60:
+# 	print("Bom exercício!")
+# else:
+# 	print("Você não pode usar este aparelho.")
+
+# meu
+# cliente = float(input("qual sua idade ? "))
+# idade_minima = 1.60
+
+# if cliente >= idade_minima:
+#     print("voce esta apto para o uso")
+# else:
+#     print("voce NAO pode usar esse aparelho!")
+
+"Exercício 3: Aplicando o cupom"
+# valor = float(input("Digite o valor total da compra: "))
+# valor_final = valor
+# tem_cupom = input("Você possui cupom de desconto? (sim/não): ")
+# if tem_cupom == "sim":
+#   cupom = input("Digite o código do cupom: ")
+#   if cupom == "LINDODEMAIS":
+#     desconto = valor * 0.10
+#     valor_final = valor - desconto
+#   else:
+#     print("Cupom inválido. Nenhum desconto aplicado.")
+
+# print("Preço final: R$", valor_final)
+
+# meu
+# compra = float(input("valor total da compra : "))
+# resultado = compra
+
+# tem_cupom =  input("possui cupom (sim/nao) : ")
+# if tem_cupom == "sim":
+#     cupom = input("seu cupom : ")
+#     if cupom == "LINDODEMAIS":
+#         desconto = resultado * 0.10   
+#         resultado = compra - desconto
+
+# else:
+#     print("inválido")
+
+# print(f"preco final {resultado}")
